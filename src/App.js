fae4f45be21b96221964295787ae2ee3610f340f
IvanZhov <izhovnych@aceproject.space>
2023-05-08 16:59:03 -0500

    Added Modal.js and Backdrop.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Modal from './components/Modal';
 import Todo from './components/Todo';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
-    <div className="">
+    <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text="Text 1"/>
+      <Todo text="Text 2"/>
+      <Todo text="Text 3"/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
