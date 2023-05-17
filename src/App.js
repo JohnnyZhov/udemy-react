d69a33624f33c3b5655c4a3b546b90bcbab44b3e
IvanZhov <izhovnych@aceproject.space>
2023-05-17 15:03:03 -0500

    Update to lesson-39

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMetup from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-        <BrowserRouter>
-        <MainNavigation />
+      <BrowserRouter>
+        <Layout>
           <Routes>
             <Route path="/" element={<AllMeetupsPage />} />
             <Route path="/new-meetup" element={<NewMetup />} />
             <Route path="/favourites" element={<Favourites />} />
           </Routes>
-        </BrowserRouter>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
