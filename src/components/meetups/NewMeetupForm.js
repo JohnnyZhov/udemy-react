ee2858b81daf63fc5b85556ca247797eae79a364
JohnnyZhov <ivan.zhovnych@gmail.com>
2023-05-17 22:15:12 -0500

    Update 1

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,24 +1,30 @@
 import Card from "../../ui/Card";
+import classes from "./NewMeetupForm.module.css";
 
 function NewMeetupForm() {
   return (
     <Card>
       <form className={classes.form}>
         <div className={classes.control}>
-            <label htmlFor="title">Meetup Title</label> //conect label to the title for screenreaders
-            <input type="text" required id="title"></input>
+          <label htmlFor="title">Meetup Title</label> //conect label to the
+          title for screenreaders
+          <input type="text" required id="title"></input>
         </div>
         <div className={classes.control}>
-            <label htmlFor="image">Meetup Image</label> //conect label to the title for screenreaders
-            <input type="url" required id="image"></input>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image"></input>
         </div>
         <div className={classes.control}>
-            <label htmlFor="address">Meetup Address</label> //conect label to the title for screenreaders
-            <input type="text" required id="address"></input>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address"></input>
         </div>
         <div className={classes.control}>
-            <label htmlFor="description">Meetup description</label> //conect label to the title for screenreaders
-            <textarea id="description" required rows='5'></textarea> //required for in browser validation
+          <label htmlFor="description">Meetup description</label>
+          <textarea id="description" required rows="5"></textarea> //required
+          for in browser validation
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
         </div>
       </form>
     </Card>
