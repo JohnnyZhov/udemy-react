57715d1b0a2fe8e74e3bf8c04c71290ca7cdd3b2
JohnnyZhov <ivan.zhovnych@gmail.com>
2023-05-19 10:55:32 -0500

    Added navigation redirecting after submit using useNavigate hook

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMetup() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-57575-default-rtdb.firebaseio.com/meetups.json",
@@ -9,7 +13,9 @@ function NewMetup() {
         body: JSON.stringify(meetupData),
         headers: { "Content-Type": "application/json" },
       }
-    );
+    ).then(()=> {
+      navigate('/');
+    });
   }
   return (
     <section>
